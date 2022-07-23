ba46f3a1e693330b116d388e3d02ab605e774e3b
ErikiCav <109443961+ErikiCav@users.noreply.github.com>
2022-07-23 13:10:05 -0300

    Correção de bug.
    O bot estava num loop duplo ao terminar de reproduzir a mídia.

--- tgcalls/tgcalls.py
+++ tgcalls/tgcalls.py
@@ -1,15 +1,10 @@
 from os import remove
 from Database import vazia, get_usada, get_lista, get_modo
 from pyrogram import Client
-import config
-import time
-from pytgcalls import PyTgCalls
-from pytgcalls import StreamType
+import config, time, telegram
+from pytgcalls import PyTgCalls, StreamType, PyTgCalls
 from pytgcalls.types import Update
-from pytgcalls import PyTgCalls
-from pytgcalls import StreamType
 from pytgcalls.types.input_stream import AudioVideoPiped, AudioPiped
-import telegram
 
 client = Client(":memory", config.API_ID, config.API_HASH, session_string=config.SESSION_STRING)
 
@@ -17,37 +12,72 @@ bot = telegram.Bot(config.TOKEN_BOT)
 
 pytgcalls = PyTgCalls(client)
 
+filter = []
+
+def CheckDual(chat_id):
+  quantidade = 0
+  for id in filter:
+    if(f"{chat_id}" in f"{id}"):
+      quantidade += 1
+  return quantidade
 
 @pytgcalls.on_stream_end()
 async def on_stream_end(clientd: PyTgCalls, update: Update):
   chat_id = update.chat_id
-  if(vazia(chat_id)):
-    try:
-      await pytgcalls.leave_group_call(chat_id)
-    except:
-      pass
-    bot.send_message(chat_id, "<b>O bot saiu da chamada pois não há nada na fila de reprodução!</b>","html")
-    local = get_usada(chat_id)[1]
-    remove(local)
+  filter.append(chat_id)
+  if(get_modo(chat_id) == "video"):
+    if(CheckDual(chat_id) == 2):
+      filter.remove(chat_id)
+      filter.remove(chat_id)
+      if(vazia(chat_id)):
+        try:
+          await pytgcalls.leave_group_call(chat_id)
+        except:
+          pass
+        bot.send_message(chat_id, "<b>O bot saiu da chamada pois não há nada na fila de reprodução!</b>","html")
+        local = get_usada(chat_id)[1]
+        remove(local)
+      else:
+        dados = get_lista(chat_id)
+        titulo = dados[1]
+        link = dados[2]
+        local = dados[3]
+        try:
+          await pytgcalls.leave_group_call(chat_id)
+        except:
+          pass
+        time.sleep(2)
+        try:
+          await pytgcalls.join_group_call(chat_id,AudioVideoPiped(local),stream_type=StreamType().pulse_stream,)
+        except:
+          await pytgcalls.join_group_call(chat_id, AudioPiped(local), stream_type=StreamType().pulse_stream, )
+        bot.send_message(chat_id, f"""<b>Próxima mídia tocando:</b> <a href="{link}">{titulo}</a>.\n\n<b>Use /listas para listar as listas de reprodução!</b>""","html")
+        local = get_usada(chat_id)[1]
+        remove(local)
+
   else:
-    dados = get_lista(chat_id)
-    titulo = dados[1]
-    link = dados[2]
-    local = dados[3]
-    try:
-      await pytgcalls.leave_group_call(chat_id)
-    except:
-      pass
-    time.sleep(2)
-    if(get_modo(chat_id)=="video"):
+    filter.remove(chat_id)
+    if(vazia(chat_id)):
       try:
-        await pytgcalls.join_group_call(chat_id,AudioVideoPiped(local),stream_type=StreamType().pulse_stream,)
+        await pytgcalls.leave_group_call(chat_id)
       except:
-        await pytgcalls.join_group_call(chat_id, AudioPiped(local), stream_type=StreamType().pulse_stream, )
+        pass
+      bot.send_message(chat_id, "<b>O bot saiu da chamada pois não há nada na fila de reprodução!</b>","html")
+      local = get_usada(chat_id)[1]
+      remove(local)
     else:
+      dados = get_lista(chat_id)
+      titulo = dados[1]
+      link = dados[2]
+      local = dados[3]
+      try:
+        await pytgcalls.leave_group_call(chat_id)
+      except:
+        pass
+      time.sleep(2)
       await pytgcalls.join_group_call(chat_id, AudioPiped(local), stream_type=StreamType().pulse_stream, )
-    bot.send_message(chat_id, f"""<b>Próxima mídia tocando:</b> <a href="{link}">{titulo}</a>.\n\n<b>Use /listas para listar as listas de reprodução!</b>""","html")
-    local = get_usada(chat_id)[1]
-    remove(local)
+      bot.send_message(chat_id, f"""<b>Próxima mídia tocando:</b> <a href="{link}">{titulo}</a>.\n\n<b>Use /listas para listar as listas de reprodução!</b>""","html")
+      local = get_usada(chat_id)[1]
+      remove(local)
 
 run = pytgcalls.start()
